128be5df9565ee9acfe78cdba35fb37487446131
swetabarman <swetabarman@gmail.com>
2025-06-23 14:54:54 -0400

    fix: Add E2E tests

---
+++ scripts/generate-access-token.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const jwtAuth = require('@adobe/jwt-auth');
+
+// Read and parse the credentials
+const fileContent = fs.readFileSync(process.env.SERVICE_CREDENTIALS_PATH, 'utf8');
+const credsRaw = JSON.parse(fileContent);
+const integration = credsRaw.integration || {};
+const technicalAccount = integration.technicalAccount || {};
+
+const config = {
+  clientId: technicalAccount.clientId,
+  clientSecret: technicalAccount.clientSecret,
+  technicalAccountId: integration.id,
+  orgId: integration.org,
+  privateKey: integration.privateKey,
+  metaScopes: [integration.metascopes], // wrap as array if it's a string
+  ims: `https://${integration.imsEndpoint}`,
+};
+
+jwtAuth(config)
+  .then((response) => {
+    // response.access_token is your access token
+    const output = `access_token=${response.access_token}\n`;
+    fs.appendFileSync(process.env.GITHUB_OUTPUT, output);
+  })
+  .catch(() => {
+    // Optionally log error (but redact sensitive info)
+    process.exit(1);
+  });
